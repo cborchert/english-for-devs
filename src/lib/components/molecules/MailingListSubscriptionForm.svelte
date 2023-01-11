f91edb1d619644d1fa55740a90cb00759f703ed7
Christopher Borchert <cborchertrun@gmail.com>
2023-01-11 22:53:47 +0100

    Mailinglist start

---
+++ src/lib/components/molecules/MailingListSubscriptionForm.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Button from '../atoms/Button.svelte';
+</script>
+
+<form>
+	<div>
+		<input type="email" placeholder="adresse e-mail" />
+	</div>
+	<fieldset>
+		<label>
+			<input type="checkbox" />
+			Je veux être informé lorsque le cours sera disponible en bêta
+		</label>
+		<label>
+			<input type="checkbox" />
+			I agree to the terms and conditions
+		</label>
+	</fieldset>
+	<div><Button>Subscribe</Button></div>
+	<p class="details">
+		Je prends au sérieux la protection de vos données personnelles et respectons votre vie privée.
+		Vous pouvez résilier votre abonnement en tout temps.
+	</p>
+</form>
+
+<style>
+	.details {
+		margin-top: var(--space-md);
+		color: var(--color-disabled);
+		font-size: var(--font-size-sm);
+	}
+</style>
--- src/routes/(pages)/+page.svelte
+++ src/routes/(pages)/+page.svelte
@@ -2,12 +2,12 @@
 	import Button from '$lib/components/atoms/Button.svelte';
 	import Card from '$lib/components/atoms/Card.svelte';
 	import AccentText from '$lib/components/atoms/AccentText.svelte';
+	import MailingListSubscriptionForm from '$lib/components/molecules/MailingListSubscriptionForm.svelte';
 </script>
 
 <Card>
-	<div>
-		<h1>Welcome to <AccentText>English for Devs</AccentText></h1>
-		<p><a href="/typography-test">Check the typography</a></p>
-		<p><Button href="/quiz-example">Take a quiz</Button></p>
-	</div>
+	<h1><AccentText>Anglais pour les devs</AccentText></h1>
+	<h4>Un cours d'anglais spécialisé pour les personnes travaillant dans l'informatique.</h4>
+	<h5>Le cours est en plein developpement, mais je peux vous dire dès qu'il sera prêt.</h5>
+	<MailingListSubscriptionForm />
 </Card>
