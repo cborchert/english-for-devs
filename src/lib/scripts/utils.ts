ed14140719d0190c76ff9820ab4fe030cfff02e0
Christopher Borchert <cborchertrun@gmail.com>
2023-01-19 16:40:30 +0100

    force node version 18

--- src/lib/scripts/utils.ts
+++ src/lib/scripts/utils.ts
@@ -1,6 +1,6 @@
 /**
  * Return a deep copy of the non plain old javascript object, serialized.
- * @see https://developer.mozilla.org/en-US/docs/Web/API/structuredClone for details on structuredClone
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/structuredClone for details on structuredClone (available starting in node 17)
  */
 export const serializeNonPOJO = (obj: any): any => {
 	// return JSON.parse(JSON.stringify(obj));
