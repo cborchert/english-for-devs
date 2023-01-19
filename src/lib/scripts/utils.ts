6141b58840db8a7492a895d9278327e98dd22f41
Christopher Borchert <cborchertrun@gmail.com>
2023-01-19 16:46:10 +0100

    stop using structuredClone and fix password issue

--- src/lib/scripts/utils.ts
+++ src/lib/scripts/utils.ts
@@ -1,8 +1,9 @@
 /**
  * Return a deep copy of the non plain old javascript object, serialized.
- * @see https://developer.mozilla.org/en-US/docs/Web/API/structuredClone for details on structuredClone (available starting in node 17)
+ *
  */
 export const serializeNonPOJO = (obj: any): any => {
-	// return JSON.parse(JSON.stringify(obj));
-	return structuredClone(obj);
+	return JSON.parse(JSON.stringify(obj));
+	// @see https://developer.mozilla.org/en-US/docs/Web/API/structuredClone for details on structuredClone (available starting in node 17)
+	// return structuredClone(obj);
 };
