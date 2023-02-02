3535675b3595e5e17893ca6b46aac1f61dde7050
Christopher Borchert <cborchertrun@gmail.com>
2023-02-02 08:23:04 +0100

    get course data from database

---
+++ src/routes/(pages)/app/+page.server.ts
@@ -0,0 +1,80 @@
+import { serializeNonPOJO } from '$lib/scripts/utils';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ locals, request }) => {
+	// load course and progress data
+
+	// TODO: add course types
+	const courses = await locals.pb.collection('courses').getFullList(200, {
+		filter: 'slug = "anglais-pour-les-devs"',
+		expand: 'modules,modules.chapters'
+	});
+
+	// TODO: add progress types
+	const progress = await locals.pb.collection('progress').getFullList(200, {
+		filter: `user.id = "${locals.pb?.authStore?.model?.id}"`,
+		expand: 'chapter'
+	});
+
+	const course = courses?.[0] || {};
+	const modules = course.expand?.modules;
+
+	const chapterProgress: { [id: string]: number } = progress.reduce((acc: any, item: any) => {
+		let progress = 0;
+		if (item.expand?.chapter?.chapterType === 'exercise') {
+			const numberOfChapterQuestions = item.expand.chapter.questions?.length || 0;
+			const numberOfCompletedQuestions = item.completedQuestions?.length || 0;
+			progress =
+				numberOfChapterQuestions > 0 ? numberOfCompletedQuestions / numberOfChapterQuestions : 0;
+		} else {
+			progress = item.progress || 0;
+		}
+		return {
+			...acc,
+			[item.expand?.chapter?.id]: progress
+		};
+	}, {});
+
+	const moduleProgress: { [id: string]: number } = modules.reduce((acc: any, module: any) => {
+		const numberOfModuleChapters = module.chapters?.length || 0;
+		const moduleChapterProgressSum = module.chapters?.reduce(
+			(acc: any, chapterId: any) => acc + chapterProgress[chapterId] || 0,
+			0
+		);
+		const moduleProgress =
+			numberOfModuleChapters > 0 ? moduleChapterProgressSum / numberOfModuleChapters : 0;
+		return {
+			...acc,
+			[module.id]: moduleProgress
+		};
+	}, {});
+
+	// TODO: add expected types
+	const sanitizedCourse = {
+		title: course.title,
+		description: course.description,
+		// TODO: add module types
+		modules: course.expand?.modules?.map((module: any) => ({
+			title: module.title,
+			subtitle: module.subtitle,
+			// TODO: determine variant 'current' | 'upcoming' | 'completed' | 'locked'
+			progress: (moduleProgress[module.id] || 0) * 100,
+			// TODO: add chapter types
+			chapters: module.expand?.chapters?.map((chapter: any) => {
+				const pageTyoe = chapter.chapterType === 'exercise' ? 'exercise' : 'lesson';
+				const href = `/app/${pageTyoe}/${chapter.slug}`;
+				return {
+					title: chapter.title,
+					description: chapter.description,
+					type: chapter.chapterType,
+					progress: (chapterProgress[chapter.id] || 0) * 100,
+					href
+				};
+			})
+		}))
+	};
+
+	return {
+		course: sanitizedCourse
+	};
+}) satisfies PageServerLoad;
--- src/routes/(pages)/app/+page.svelte
+++ src/routes/(pages)/app/+page.svelte
@@ -1,81 +1,13 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+
 	import Container from '$lib/components/atoms/Container.svelte';
 	import Module from '$lib/components/molecules/Module.svelte';
 
-	const chapters = [
-		{
-			type: 'video',
-			title: 'Introduction and Objectives',
-			progress: 100,
-			href: '#'
-		},
-		{
-			type: 'text',
-			title: 'Greetings and Introductions',
-			progress: 100,
-			href: '#'
-		},
-		{
-			type: 'exercise',
-			title: 'Exercise 1',
-			progress: 70,
-			href: 'app/exercise/1'
-		},
-		{
-			type: 'exercise',
-			title: 'Exercise 2',
-			progress: 0,
-			href: 'app/exercise/1'
-		},
-		{
-			type: 'audio',
-			title: 'A Simple Conversation',
-			progress: 0,
-			href: '#'
-		},
-		{
-			type: 'reading',
-			title: 'A Simple Story',
-			progress: 0,
-			href: '#'
-		},
-		{
-			type: 'list',
-			title: 'Recap',
-			progress: 0,
-			href: '#'
-		}
-	];
-	const modules = [
-		{
-			title: 'Module 1',
-			subtitle: 'Saying Hello',
-			variant: 'completed',
-			showReview: true,
-			progress: 100,
-			chapters
-		} as const,
-		{
-			title: 'Module 2',
-			subtitle: "How's It Going?",
-			variant: 'current',
-			showReview: true,
-			progress: 50,
-			chapters
-		} as const,
-		{
-			title: 'Module 3',
-			subtitle: 'Daily Stand Up',
-			variant: 'upcoming',
-			chapters
-		} as const,
-		{
-			title: 'Module 4',
-			subtitle: 'Code Review',
-			variant: 'locked',
-			chapters
-		} as const
-	];
+	export let data: PageData;
+
+	const { course } = data;
+	const { modules = [] } = course?.modules || {};
 </script>
 
 <Container size="sm">
